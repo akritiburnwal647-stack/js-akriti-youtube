77be2b24d7850da79f258a1a43adde973418788b
Akriti <akritiburnwal647@gmail.com>
2026-04-03 22:20:46 +0530

    conversion

--- datatype.js
+++ datatype.js
@@ -10,3 +10,31 @@ let somenum=33
 let stringnumber=String(somenum)
 console.log(stringnumber);
 console.log( typeof stringnumber);
+                       //operatios//
+let value=3
+let negvalue=-value;
+console.log(negvalue);
+let str1="hello"
+let str2="akriti"
+let str3= str1 + " "+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"3");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter);
+
+
+
+
