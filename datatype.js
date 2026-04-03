65edaa13f7866f181804b578d1941cfd91db1f19
Akriti <akritiburnwal647@gmail.com>
2026-04-03 22:03:33 +0530

    conversion of datatype

---
+++ datatype.js
@@ -0,0 +1,12 @@
+let score="32abs"
+console.log(typeof score);
+let valueInnumber= Number(score)
+console.log(typeof valueInnumber);
+console.log( valueInnumber);
+let isLoggedIn=1
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+let somenum=33
+let stringnumber=String(somenum)
+console.log(stringnumber);
+console.log( typeof stringnumber);
