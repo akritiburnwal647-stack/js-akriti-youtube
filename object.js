72c899c2b3b2eb7afc93d41057d3bbd95437091b
Akriti <akritiburnwal647@gmail.com>
2026-05-18 17:44:59 +0530

    added object file

---
+++ object.js
@@ -0,0 +1,21 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseinstructor: "hitesh"
+}
+const { courseinstructor: instructor } = course
+//console.log(courseinstructor);
+//if we want destructure the sentence and dont want to write a such long name like courseinstructor 
+console.log(instructor);
+//API(put your work on someone else head)
+{
+    // "name": "hitesh",
+    //      "coursename": "js in hindi",
+    //           "price": "free"
+}//json format
+[
+    {},
+    {},
+    {}
+]
+
